e077ea9580d63cc2de4efd9c963126db09d58807
chinyancb <chinyancb@yahoo.co.jp>
2018-08-16 21:35:31 +0900

    bug fix

--- R/simpleLm.R
+++ R/simpleLm.R
@@ -14,27 +14,38 @@ simpleLm <- function(dataset, target='target', family='binomial') {
   cf.stderr <- NULL
   cf.zvalue <- NULL
   cf.pr     <- NULL
+  cf.deviance <- NULL
 
-  for (feature in colnames(dataset)) {
+  # 目的変数の有無確認
+  if (!sum(colnames(dataset) == target)) {
+    cat(sprintf("not found target variable. [%s]\n", target))
+    return(FALSE)
+  }
 
-    if (feature == target) next
+  # 目的変数のindex
+  tgt_index <- which(colnames(dataset) == target)
+
+  for (n_index in 1:length(colnames(dataset))) {
+
+    if (n_index == tgt_index) next
 
     # 単回帰モデル
-    tmp_df <- dataset %>% dplyr::select(target, feature)
-    model <- glm(tmp_df[, c(target)] ~ tmp_df[, c(feature)], data = tmp_df, family = 'binomial')
+    tmp_df <- dataset %>% dplyr::select(tgt_index, n_index)
+    model <- glm(tmp_df[, 1] ~ tmp_df[, 2], data = tmp_df, family = 'binomial')
 
     # 回帰係数の情報を取得
-    cf.name   <- append(cf.name, feature)
+    cf.name   <- append(cf.name, colnames(dataset)[n_index])
     cf.est    <- append(cf.est, summary(model)$coefficients[2])
     cf.stderr <- append(cf.stderr, summary(model)$coefficients[4])
     cf.zvalue <- append(cf.zvalue, summary(model)$coefficients[6])
     cf.pr     <- append(cf.pr, summary(model)$coefficients[8])
+    cf.deviance <- append(cf.deviance, summary(model)$deviance)
     rm(tmp_df)
   }
 
   # 結果を整える
-  ret <- data.frame(cf.name, cf.est, cf.stderr, cf.zvalue, cf.pr) %>%
-    dplyr::arrange(cf.pr) %>%
+  ret <- data.frame(cf.name, cf.est, cf.stderr, cf.zvalue, cf.deviance, cf.pr) %>%
+    dplyr::arrange(cf.deviance, cf.pr) %>%
     dplyr::mutate(signif = dplyr::case_when(
       is.na(cf.pr)    ~ 'NA',
       cf.pr <= 0.001  ~ '***',
